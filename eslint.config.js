7f884ff8d6b854f4c5b100133d78cc6b4dab385e
NoName <mostblack4@gmail.com>
2026-09-08 19:45:56 +0300

    build: initialize project dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import SimpleImportSort from "eslint-plugin-simple-import-sort";
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
@@ -17,6 +18,20 @@ export default defineConfig([
     ],
     languageOptions: {
       globals: globals.browser,
+    },
+
+    plugins: { "simple-import-sort": SimpleImportSort },
+    rules: {
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            ["^react$", "^react-dom$", "^react"],
+            ["^@/"],
+            ["^\\.\\.", "^\\."],
+          ],
+        },
+      ],
     },
   },
 ])
