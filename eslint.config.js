357d84dabecdf9957e1ac2e7aebc7ada97e29119
NoName <mostblack4@gmail.com>
2026-09-10 13:18:41 +0300

    chore: create path aliases

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,15 @@
-import js from '@eslint/js'
-import globals from 'globals'
+import js from "@eslint/js";
+import globals from "globals";
 import SimpleImportSort from "eslint-plugin-simple-import-sort";
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -34,4 +34,4 @@ export default defineConfig([
       ],
     },
   },
-])
+]);
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vitest/config";
+/// <reference types="vitest/config" />
 
-// https://vite.dev/config/
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+
 export default defineConfig({
   plugins: [react()],
+
+  resolve: {
+    alias: {
+      "@app": path.resolve(__dirname, "./src/app"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@widgets": path.resolve(__dirname, "./src/widgets"),
+      "@features": path.resolve(__dirname, "./src/features"),
+      "@entities": path.resolve(__dirname, "./src/entities"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+    },
+  },
+
   test: {
     globals: true,
     environment: "jsdom",
