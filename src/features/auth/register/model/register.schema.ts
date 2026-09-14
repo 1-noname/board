81b1819b7632665574bec2723bcec125dfea4639
NoName <mostblack4@gmail.com>
2026-09-14 12:28:29 +0300

    refactor(validation): replace deprecated email validation

--- src/features/auth/register/model/register.schema.ts
+++ src/features/auth/register/model/register.schema.ts
@@ -3,11 +3,7 @@ import { z } from "zod";
 export const registerSchema = z
   .object({
     name: z.string().trim().min(1, "Name is required"),
-    email: z
-      .string()
-      .trim()
-      .min(1, "Email is required")
-      .pipe(z.email("Invalid email address")),
+    email: z.email("Invalid email address").trim().min(1, "Email is required"),
     password: z
       .string()
       .min(1, "Password is required")
