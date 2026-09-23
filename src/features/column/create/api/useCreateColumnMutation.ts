c1df5a343a02020e6eaf3a95de08b4d81d4d9e90
NoName <mostblack4@gmail.com>
2026-09-23 16:21:47 +0300

    feat(column): add api hook for creating columns

---
+++ src/features/column/create/api/useCreateColumnMutation.ts
@@ -0,0 +1,29 @@
+import { createColumnSchema } from "../model/schema";
+
+import { api } from "@shared/api/base";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+interface createColumnRequestProps {
+  boardId: string;
+  title: string;
+}
+
+const createColumnRequest = async ({
+  boardId,
+  title,
+}: createColumnRequestProps) => {
+  const { data } = await api.post(`/boards/${boardId}/columns`, { title });
+
+  return createColumnSchema.parse(data);
+};
+
+export const useCreateColumnMutation = (boardId: string) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (title: string) => createColumnRequest({ boardId, title }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["columns", boardId] });
+    },
+  });
+};
