bc77715badd52f16421b5d8dfd687d870fd81823
NoName <mostblack4@gmail.com>
2026-09-24 16:45:45 +0300

    fix: remove await before cancelQueries for fix UI twitching

--- src/features/column/reorder/api/useReorderColumnMutation.ts
+++ src/features/column/reorder/api/useReorderColumnMutation.ts
@@ -1,3 +1,4 @@
+import { arrayMove } from "@dnd-kit/sortable";
 import type { Column } from "@entities/column";
 import { api } from "@shared/api/base";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
@@ -8,29 +9,25 @@ interface ReorderColumnParams {
   newOrder: number;
 }
 
-type ReorderColumnPayload = Omit<ReorderColumnParams, "boardId">;
-
-const reorderColumnRequest = async ({
-  boardId,
-  columnId,
-  newOrder,
-}: ReorderColumnParams) => {
-  const { data } = await api.patch(
-    `/boards/${boardId}/columns/${columnId}/order`,
-    { newOrder },
-  );
-
-  return data;
-};
-
 export const useReorderColumnMutation = (boardId: string) => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: (payload: ReorderColumnPayload) =>
-      reorderColumnRequest({ boardId, ...payload }),
+    mutationFn: async ({
+      columnId,
+      newOrder,
+    }: Omit<ReorderColumnParams, "boardId">) => {
+      const { data } = await api.patch(
+        `/boards/${boardId}/columns/${columnId}/order`,
+        {
+          newOrder,
+        },
+      );
+      return data;
+    },
+
     onMutate: async ({ columnId, newOrder }) => {
-      await queryClient.cancelQueries({ queryKey: ["columns", boardId] });
+      queryClient.cancelQueries({ queryKey: ["columns", boardId] });
 
       const prevColumns = queryClient.getQueryData<Column[]>([
         "columns",
@@ -38,31 +35,23 @@ export const useReorderColumnMutation = (boardId: string) => {
       ]);
 
       if (prevColumns) {
-        const oldIndex = prevColumns.findIndex(
-          (column) => column.id === columnId,
-        );
+        const oldIndex = prevColumns.findIndex((col) => col.id === columnId);
 
-        if (oldIndex == -1) {
-          const updatedColumns = [...prevColumns];
-
-          const [movedColumn] = updatedColumns.splice(oldIndex, 1);
-
-          updatedColumns.splice(newOrder, 0, movedColumn);
-
+        if (oldIndex !== -1) {
+          const updatedColumns = arrayMove(prevColumns, oldIndex, newOrder);
           queryClient.setQueryData(["columns", boardId], updatedColumns);
         }
       }
 
       return { prevColumns };
     },
-    onError: (_err, _variable, omMutateResult) => {
-      if (omMutateResult?.prevColumns) {
-        queryClient.setQueryData(
-          ["columns", boardId],
-          omMutateResult.prevColumns,
-        );
+
+    onError: (_err, _variables, onMutate) => {
+      if (onMutate?.prevColumns) {
+        queryClient.setQueryData(["columns", boardId], onMutate.prevColumns);
       }
     },
+
     onSettled: () => {
       queryClient.invalidateQueries({ queryKey: ["columns", boardId] });
     },
